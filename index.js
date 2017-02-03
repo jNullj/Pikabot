33c6bc65454b1b733d761908dda98f83a2322b60
jNullj <iontankatchker@gmail.com>
2017-02-04 00:03:41 +0200

    Cheak everything ok before running the bot

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const TOKEN = 'put bot token here';
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
+// login the bot
 client.login(TOKEN);
+// critical, makes sure the bot will only start after discord gives it the green flag
+bot.on('ready', () => {
+  console.log('I am ready!');
+});

45fbac592989cd7e5f73b57d4a0ce05c0fb7fcab
jNullj <iontankatchker@gmail.com>
2017-02-04 01:56:03 +0200

    Fixed login error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TOKEN = 'put bot token here';
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 // login the bot
-client.login(TOKEN);
+bot.login(TOKEN);
 // critical, makes sure the bot will only start after discord gives it the green flag
 bot.on('ready', () => {
   console.log('I am ready!');

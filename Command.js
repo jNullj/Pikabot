6fbca3b56ebbe0b91eb92a291a95d006d1ac79d6
jNullj <iontankatchker@gmail.com>
2017-02-04 01:55:30 +0200

    initiated the command class

---
+++ Command.js
@@ -0,0 +1,12 @@
+function Command(input){
+    this.command = input;
+}
+
+// GET
+Command.prototype.getCommand = function(){
+    return this.command;
+}
+
+Command.prototype.getPoints(){
+    // TODO
+}

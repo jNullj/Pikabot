736f93cfc4b9620a2a70e1f4de0f3d98c9f0f041
jNullj <iontankatchker@gmail.com>
2017-02-04 13:01:58 +0200

    Added cheak for new users

--- Command.js
+++ Command.js
@@ -3,7 +3,14 @@ const User = require('./User.js');
 class Command {
     static getPoints(id){
         var user = new User(id);
-        user.load();
+        if(!user.isExists()){
+            if (user.getID() < 0) {
+                throw 'invalid user id';
+            }
+            user.save();
+        }else{
+            user.load();
+        }
         return user.getPoints();
     }
 }

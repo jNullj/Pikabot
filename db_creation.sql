1397ca18c3d9d7dc5c0b8845f6f88674c73dbc8b
jNullj <iontankatchker@gmail.com>
2017-11-09 21:35:31 +0200

    Added a file to create the DB if its missing

---
+++ db_creation.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+	id INTEGER NOT NULL,
+	points INTEGER,
+	birthday INTEGER
+);
+
+CREATE UNIQUE INDEX users_id_IDX ON users (id);
+
+CREATE TABLE gods (
+	id INTEGER NOT NULL,
+	name INTEGER NOT NULL,
+	points INTEGER
+);
+
+CREATE UNIQUE INDEX gods_id_IDX ON gods (id);
+

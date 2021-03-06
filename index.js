1087652132a507f7b4b71474a4c32193222ae0ed
jNullj <iontankatchker@gmail.com>
2021-03-06 11:05:01 +0200

    Added new server user welcome message
    When a new user joins pikabot will now welcome them, with his tender voice: "pika pika, pika pik".
    His thunder would hit the new user that just joined the server, and re-birth him as follower of the lord pika.
    
    This should be part of the solution for issue #10

--- index.js
+++ index.js
@@ -81,3 +81,13 @@ function bdayHandler(message){
     // update last cheak date to today
     bday_cheak = today;
 }
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});

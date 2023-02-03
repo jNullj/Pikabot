ef65d5c129ed0b1a126844050533d118b1e712e7
jNullj <jNullj@users.noreply.github.com>
2023-02-03 23:58:14 +0200

    Added slash commands handler
    Following the guide at https://discordjs.guide/creating-your-bot/command-handling.html#loading-command-files.
    Pikabot will use the recommended structure and load slash commands from the commands folder as modules.
    
    Part of issue #18

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // for testing/easy configuration
 const botinfo = require("./botinfo.js");
+// for loading command files
+const fs = require('node:fs');
+const path = require('node:path');
 
 const TOKEN = botinfo.token;
 const Discord = require('discord.js');
@@ -10,6 +13,42 @@ myIntents.add(Discord.IntentsBitField.Flags.GuildMembers);
 myIntents.add(Discord.IntentsBitField.Flags.Guilds);
 myIntents.add(Discord.IntentsBitField.Flags.MessageContent);
 const bot = new Discord.Client({ intents: myIntents });
+
+// slash commands handler
+bot.commands = new Discord.Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		bot.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+bot.on(Discord.Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 // login the bot
 bot.login(TOKEN);
 // critical, makes sure the bot will only start after discord gives it the green flag

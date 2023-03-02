39bb014a675782040bfd92d3c703228a65829397
jNullj <jNullj@users.noreply.github.com>
2023-03-02 23:34:10 +0200

    refactor: Move ready event handler to dedicated file

--- index.js
+++ index.js
@@ -65,10 +65,6 @@ for (const file of eventFiles) {
 
 // login the bot
 bot.login(TOKEN);
-// critical, makes sure the bot will only start after discord gives it the green flag
-bot.on('ready', () => {
-  console.log('I am ready!');
-});
 // Load the user class
 const User = require('./User.js');
 // Load commands class

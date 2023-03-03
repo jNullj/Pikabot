1c43cf0365de0e691717527ea03a7a023879c565
jNullj <jNullj@users.noreply.github.com>
2023-03-03 12:31:03 +0200

    refactor: welcome message event handler and move to dedicated file
    - Remove welcome message event handler from index.js
    - Create a new event handler file for the `GuildMemberAdd` event
    - Move the welcome message event handler logic to the new file
    - Update the welcome message event handler to use a constant variable for the channel name, making it easier to maintain

--- index.js
+++ index.js
@@ -118,16 +118,6 @@ function bdayHandler(message){
     bday_cheak = today;
 }
 
-// Create an event listener for new guild members
-bot.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-});
-
 bot.on('voiceStateUpdate', (oldState, newState) => {
 
   let oldChannel = oldState.channelId;

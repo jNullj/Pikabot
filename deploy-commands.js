eb32525ebd3448192f605c55e73f7fbdc9c86873
jNullj <15849761+jNullj@users.noreply.github.com>
2024-05-24 22:58:32 +0300

    fix: commands depoly env veriables

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,7 +12,7 @@ for (const file of commandFiles) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.API_KEY);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_API_TOKEN);
 
 // and deploy your commands!
 (async () => {

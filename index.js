77aa8465c835140a4529c007e535e0505927432e
jNullj <15849761+jNullj@users.noreply.github.com>
2025-03-29 16:26:44 +0300

    Update interaction replies to use MessageFlags for ephemeral responses
    removes Deprecated emphemral warning

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ bot.on(Discord.Events.InteractionCreate, async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		await interaction.reply({ content: 'There was an error while executing this command!', flags: Discord.MessageFlags.Ephemeral });
 	}
 });
 
